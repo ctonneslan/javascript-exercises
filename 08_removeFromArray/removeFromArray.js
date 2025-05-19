7983483b2acbb30987d11b35defe88841c874266
Charlie Tonneslan <cst0520@gmail.com>
2025-05-19 09:28:34 -0400

    Finished exercises up to and including 11.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,13 +1,5 @@
-const removeFromArray = function(...args) {
-    arr = args[0];
-    args = args.slice(1);
-    for (let i = 0; i < arr.length; i++) {
-        if (args.includes(arr[i])) {
-            arr.splice(i, 1);
-            i--;
-        }
-    }
-    return arr;
+const removeFromArray = function(array, ...args) {
+    return array.filter(item => !args.includes(item));
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (!(typeof(a) === 'number' && Math.floor(a) === a 
+        && Math.floor(b) === b && typeof(b) === 'number'
+        && a > 0 && b > 0)) {
+        return 'ERROR'
+    }
+    let sum = 0;
+    for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
