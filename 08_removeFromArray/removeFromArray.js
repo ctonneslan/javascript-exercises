27ff40898c5361ab98d0b22d2dd95883de9afa7b
Charlie Tonneslan <cst0520@gmail.com>
2025-05-18 22:05:02 -0400

    Completed exercises 1-8.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    arr = args[0];
+    args = args.slice(1);
+    for (let i = 0; i < arr.length; i++) {
+        if (args.includes(arr[i])) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
